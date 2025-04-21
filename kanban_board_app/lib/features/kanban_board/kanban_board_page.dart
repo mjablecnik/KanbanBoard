7e0076c240d9f7e2a712270f456a9805ecb3d8a2
Martin Jablečník <martin.jablecnik@email.cz>
2025-04-21 20:09:50 +0200

    refactor(kanban_board_app): move data into repository

--- kanban_board_app/lib/features/kanban_board/kanban_board_page.dart
+++ kanban_board_app/lib/features/kanban_board/kanban_board_page.dart
@@ -1,4 +1,5 @@
 import 'package:kanban_board_app/features/kanban_board/components/board_header.dart';
+import 'package:kanban_board_app/features/kanban_board/kanban_board_repository.dart';
 import 'package:kanban_board_design/kanban_board_design.dart';
 import 'package:appflowy_board/appflowy_board.dart';
 import 'package:vader_app/vader_app.dart';
@@ -45,54 +46,12 @@ class _KanbanBoardPageState extends State<KanbanBoardPage> {
   void initState() {
     super.initState();
     boardController = AppFlowyBoardScrollController();
-    final group1 = AppFlowyGroupData(
-      id: "To Do",
-      name: "To Do",
-      items: [
-        CardState(title: "Card 1"),
-        CardState(title: "Card 2"),
-        CardState(title: "Card 3", subtitle: 'Aug 1, 2020 4:05 PM'),
-        CardState(title: "Card 4"),
-        CardState(title: "Card 5"),
-      ],
-    );
 
-    final group2 = AppFlowyGroupData(
-      id: "In Progress",
-      name: "In Progress",
-      items: <AppFlowyGroupItem>[
-        CardState(title: "Card 6"),
-        CardState(title: "Card 7", subtitle: 'Aug 1, 2020 4:05 PM'),
-        CardState(title: "Card 8", subtitle: 'Aug 1, 2020 4:05 PM'),
-      ],
-    );
-
-    final group3 = AppFlowyGroupData(
-      id: "Pending",
-      name: "Pending",
-      items: <AppFlowyGroupItem>[
-        CardState(title: "Card 9"),
-        CardState(title: "Card 10", subtitle: 'Aug 1, 2020 4:05 PM'),
-        CardState(title: "Card 11"),
-        CardState(title: "Card 12"),
-      ],
-    );
-    final group4 = AppFlowyGroupData(
-      id: "Canceled",
-      name: "Canceled",
-      items: <AppFlowyGroupItem>[CardState(title: "Card 13"), CardState(title: "Card 14"), CardState(title: "Card 15")],
-    );
-    final group5 = AppFlowyGroupData(
-      id: "Urgent",
-      name: "Urgent",
-      items: <AppFlowyGroupItem>[CardState(title: "Card 14"), CardState(title: "Card 15")],
-    );
-
-    controller.addGroup(group1);
-    controller.addGroup(group2);
-    controller.addGroup(group3);
-    controller.addGroup(group4);
-    controller.addGroup(group5);
+    injector.use<KanbanBoardRepository>().getBoardData().then((groupData) {
+      for (var group in groupData) {
+        controller.addGroup(group);
+      }
+    });
   }
 
   @override
--- kanban_board_app/lib/features/kanban_board/kanban_board_repository.dart
+++ kanban_board_app/lib/features/kanban_board/kanban_board_repository.dart
@@ -1,3 +1,5 @@
+import 'package:appflowy_board/appflowy_board.dart';
+import 'package:kanban_board_app/features/kanban_board/logic/card_state.dart';
 import 'package:vader_app/vader_app.dart';
 
 class KanbanBoardRepository extends Repository {
@@ -6,7 +8,50 @@ class KanbanBoardRepository extends Repository {
     required super.storageClient,
   });
 
-  Future<String> getSomething() async {
-    return Future.value('Hello world');
+  Future<List<AppFlowyGroupData>> getBoardData() async {
+    final group1 = AppFlowyGroupData(
+      id: "To Do",
+      name: "To Do",
+      items: [
+        CardState(title: "Card 1"),
+        CardState(title: "Card 2"),
+        CardState(title: "Card 3", subtitle: 'Aug 1, 2020 4:05 PM'),
+        CardState(title: "Card 4"),
+        CardState(title: "Card 5"),
+      ],
+    );
+
+    final group2 = AppFlowyGroupData(
+      id: "In Progress",
+      name: "In Progress",
+      items: <AppFlowyGroupItem>[
+        CardState(title: "Card 6"),
+        CardState(title: "Card 7", subtitle: 'Aug 1, 2020 4:05 PM'),
+        CardState(title: "Card 8", subtitle: 'Aug 1, 2020 4:05 PM'),
+      ],
+    );
+
+    final group3 = AppFlowyGroupData(
+      id: "Pending",
+      name: "Pending",
+      items: <AppFlowyGroupItem>[
+        CardState(title: "Card 9"),
+        CardState(title: "Card 10", subtitle: 'Aug 1, 2020 4:05 PM'),
+        CardState(title: "Card 11"),
+        CardState(title: "Card 12"),
+      ],
+    );
+    final group4 = AppFlowyGroupData(
+      id: "Canceled",
+      name: "Canceled",
+      items: <AppFlowyGroupItem>[CardState(title: "Card 13"), CardState(title: "Card 14"), CardState(title: "Card 15")],
+    );
+    final group5 = AppFlowyGroupData(
+      id: "Urgent",
+      name: "Urgent",
+      items: <AppFlowyGroupItem>[CardState(title: "Card 14"), CardState(title: "Card 15")],
+    );
+
+    return [group1, group2, group3, group4, group5];
   }
 }
